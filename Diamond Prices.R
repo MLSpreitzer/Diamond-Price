44278f942d955523fcf3e43c6026ab0d4a9f48ab
Monika Spreitzer <39218558+MLSpreitzer@users.noreply.github.com>
2020-10-05 10:49:14 -0500

    Add files via upload

---
+++ Diamond Prices.R
@@ -0,0 +1,19 @@
+mydata <- read.csv(file.choose())
+install.packages("ggplot2")
+install.packages("labeling")
+install.packages("farver")
+
+library(ggplot2)
+
+ggplot(data=mydata, aes(x=carat, y=price, color=clarity)) +
+  geom_point()
+
+ggplot(data=mydata, aes(x=carat, y=price, color=clarity)) +
+  geom_point(alpha=0.1)
+
+ggplot(data=mydata[mydata$carat<2.5,], aes(x=carat, y=price, color=clarity)) +
+  geom_point()#alpha=0.1)
+
+ggplot(data=mydata[mydata$carat<2.5,], aes(x=carat, y=price, color=clarity)) +
+  geom_point(alpha=0.1) +
+  geom_smooth()
